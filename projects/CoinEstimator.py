6d3a1c7d9d33ef9c53b817a1ae9de46c41db549e
tteddy7 <tteddy7@me.com>
2019-02-11 14:58:26 -0600

    adding basic info

--- projects/CoinEstimator.py
+++ projects/CoinEstimator.py
@@ -8,5 +8,24 @@
 #   Round all numbers printed out to the nearest whole number.
 #   Allow the user to select whether they want to submit the weight in either grams or pounds.
 
-def estimateWeight():
+def estimateWeight(pWgt, nWgt, dWgt, qWgt):
+  # Parse arguments
+  pWgt = float(pWgt)
+  nWgt = float(nWgt)
+  dWgt = float(dWgt)
+  qWgt = float(qWgt)
   
+  # Define weights 
+  wgtPerPenny = 2.5
+  wgtPerNickel = 5.0
+  wgtPerDime = 2.268
+  wgtPerQuarter = 2.67
+
+  # Define amounts per wrapper
+  wrapperCapacityP = 50
+  wrapperCapacityN = 40
+  wrapperCapacityD = 50
+  wrapperCapacityQ = 40
+
+  # Calculate number of coins for each weight
+  pNum = pWgt / wgtPerPenny
