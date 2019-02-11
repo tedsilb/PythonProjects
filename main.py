cf1f1fd42f9e72b1b6c1a56f43b35be989527e31
tteddy7 <tteddy7@me.com>
2019-02-11 13:16:55 -0600

    fixed the second-to-last message

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 def gen_lyrics():
   for i in range(99, 0, -1):
-    if (i == 1):
+    if (i == 2):
+      print(f'{i} bottles of beer on the wall, {i} bottles of beer!')
+      print(f'Take one down, pass it around, {i - 1} bottle of beer on the wall!')
+    elif (i == 1):
       print(f'{i} bottle of beer on the wall, {i} bottle of beer!')
-      print('Take it down, pass it around, no bottles of beer on the wall!')
+      print('Take it down, pass it around, no more bottles of beer on the wall!')
     else:
       print(f'{i} bottles of beer on the wall, {i} bottles of beer!')
       print(f'Take one down, pass it around, {i - 1} bottles of beer on the wall!')
