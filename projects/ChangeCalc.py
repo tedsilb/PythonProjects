32dbc2fc0889dd3f07b1aad692561ce529540fa8
tteddy7 <tteddy7@me.com>
2019-02-12 10:53:23 -0600

    finish code

--- projects/ChangeCalc.py
+++ projects/ChangeCalc.py
@@ -5,6 +5,9 @@ import math
 
 # Define function
 def calcChange(price):
+  # Parse price into float
+  price = float(price)
+
   # Set value of coins
   valueQ = 0.25
   valueD = 0.10
@@ -15,7 +18,35 @@ def calcChange(price):
   remainingValue = price
 
   # Determine number of quarters
-  numQ = math.floor(price / valueQ)
+  numQ = math.floor(remainingValue / valueQ)
 
   # Subtract quarters from remaining value
   remainingValue -= (numQ * valueQ)
+
+  # Determine number of dimes
+  numD = math.floor(remainingValue / valueD)
+
+  # Subtract dimes from remaining value
+  remainingValue -= (numD * valueD)
+
+  # Determine number of nickels
+  numN = math.floor(remainingValue / valueN)
+
+  # Subtract nickels from remaining value
+  remainingValue -= (numN * valueN)
+
+  # Determine number of pennies
+  numP = math.floor(remainingValue / valueP)
+
+  # Subtract pennies from remaining value
+  remainingValue -= (numP * valueP)
+
+  # Print values to console
+  print(f'For ${price}, you will need {numQ} quarters, {numD} dimes, {numN} nickels, and {numP} pennies.')
+
+# Gather data from user
+print('Please enter a price (no dollar sign), and this program will tell you what combination of coins you will need.')
+userPrice = input('Enter price: ')
+
+# Run function with user data
+calcChange(userPrice)
