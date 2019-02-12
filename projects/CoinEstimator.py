83edede7d92c04865d533c24a1542f3f6780a4fc
tteddy7 <tteddy7@me.com>
2019-02-12 10:07:50 -0600

    added tweaks, logic on return values

--- projects/CoinEstimator.py
+++ projects/CoinEstimator.py
@@ -1,12 +1,14 @@
-# Goal 
-#   Create a program that allows the user to input the total weight of each type of coin they have 
-#   (pennies, nickels, dimes, and quarters), 
-#   and then print out how many of each type of wrapper they would need, 
+# Goal
+#   Create a program that allows the user to input the total weight of each type of coin they have
+#   (pennies, nickels, dimes, and quarters),
+#   and then print out how many of each type of wrapper they would need,
 #   how many coins they have, and the estimated total value of all of their money.
 #   Weight of each coin and how many fit inside each type of wrapper.
 # Subgoals
 #   Round all numbers printed out to the nearest whole number.
 #   Allow the user to select whether they want to submit the weight in either grams or pounds.
+
+import math
 
 def estimateWeight(wgtP, wgtN, wgtD, wgtQ):
   # Parse arguments
@@ -14,14 +16,14 @@ def estimateWeight(wgtP, wgtN, wgtD, wgtQ):
   wgtN = float(wgtN)
   wgtD = float(wgtD)
   wgtQ = float(wgtQ)
-  
+
   # Define values
   valPerP = 0.01
   valPerN = 0.05
   valPerD = 0.10
   valPerQ = 0.25
 
-  # Define weights 
+  # Define weights
   wgtPerP = 2.5
   wgtPerN = 5.0
   wgtPerD = 2.268
@@ -39,17 +41,17 @@ def estimateWeight(wgtP, wgtN, wgtD, wgtQ):
   numD = wgtD / wgtPerD
   numQ = wgtQ / wgtPerQ
 
-  # Parse number of coints to integers
-  numP = int(numP)
-  numN = int(numN)
-  numD = int(numD)
-  numQ = int(numQ)
+  # Parse number of coins to integers (round down)
+  numP = math.floor(numP)
+  numN = math.floor(numN)
+  numD = math.floor(numD)
+  numQ = math.floor(numQ)
 
-  # Determine number of wrappers needed
-  wrappersP = numP / wrapperCapacityP
-  wrappersN = numN / wrapperCapacityN
-  wrappersD = numD / wrapperCapacityD
-  wrappersQ = numQ / wrapperCapacityQ
+  # Determine number of wrappers needed (round up)
+  wrappersP = math.ceil(numP / wrapperCapacityP)
+  wrappersN = math.ceil(numN / wrapperCapacityN)
+  wrappersD = math.ceil(numD / wrapperCapacityD)
+  wrappersQ = math.ceil(numQ / wrapperCapacityQ)
 
   # Determine value of currency
   valueP = numP * valPerP
@@ -58,9 +60,63 @@ def estimateWeight(wgtP, wgtN, wgtD, wgtQ):
   valueQ = numQ * valPerQ
   valueTotal = valueP + valueN + valueD + valueQ
 
+  # Print a blank line before returning values
+  print('')
+
   # Print the return values for the user
-  print(f'You have {numP} pennies, and you will need {wrappersP} for them.')
-  print(f'You have {numN} nickels, and you will need {wrappersN} for them.')
-  print(f'You have {numD} dimes, and you will need {wrappersD} for them.')
-  print(f'You have {numQ} quarters, and you will need {wrappersQ} for them.')
-  print(f'The total value of your coins is {valueTotal}.')
+  # Pennies
+  if (numP == 1):
+    if (wrappersP == 1):
+      print(f'You have {numP} penny, and you will need {wrappersP} wrapper for them.')
+    else:
+      print(f'You have {numP} penny, and you will need {wrappersP} wrappers for them.')
+  else:
+    if (wrappersP == 1):
+      print(f'You have {numP} pennies, and you will need {wrappersP} wrapper for them.')
+    else:
+      print(f'You have {numP} pennies, and you will need {wrappersP} wrappers for them.')
+  # Nickels
+  if (numN == 1):
+    if (wrappersN == 1):
+      print(f'You have {numN} nickel, and you will need {wrappersN} wrapper for them.')
+    else:
+      print(f'You have {numN} nickel, and you will need {wrappersN} wrappers for them.')
+  else:
+    if (wrappersN == 1):
+      print(f'You have {numN} nickels, and you will need {wrappersN} wrapper for them.')
+    else:
+      print(f'You have {numN} nickels, and you will need {wrappersN} wrappers for them.')
+  # Dimes
+  if (numD == 1):
+    if (wrappersD == 1):
+      print(f'You have {numD} dime, and you will need {wrappersD} wrapper for them.')
+    else:
+      print(f'You have {numD} dime, and you will need {wrappersD} wrappers for them.')
+  else:
+    if (wrappersD == 1):
+      print(f'You have {numD} dimes, and you will need {wrappersD} wrapper for them.')
+    else:
+      print(f'You have {numD} dimes, and you will need {wrappersD} wrappers for them.')
+  # Quarters
+  if (numQ == 1):
+    if (wrappersQ == 1):
+      print(f'You have {numQ} quarter, and you will need {wrappersQ} wrapper for them.')
+    else:
+      print(f'You have {numQ} quarter, and you will need {wrappersQ} wrappers for them.')
+  else:
+    if (wrappersQ == 1):
+      print(f'You have {numQ} quarters, and you will need {wrappersQ} wrapper for them.')
+    else:
+      print(f'You have {numQ} quarters, and you will need {wrappersQ} wrappers for them.')
+  # Value of coins
+  print(f'The total value of your coins is ${valueTotal}.')
+
+# Gather data from users
+print('Please enter the weights of your coins (in grams):')
+usrWgtP = input('What is the weight of your pennies? ')
+usrWgtN = input('What is the weight of your nickels? ')
+usrWgtD = input('What is the weight of your dimes? ')
+usrWgtQ = input('What is the weight of your quarters? ')
+
+# Run function
+estimateWeight(usrWgtP, usrWgtN, usrWgtD, usrWgtQ)
