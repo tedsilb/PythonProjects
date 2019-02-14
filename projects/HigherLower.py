316e7981def9b23773a9f313a6d42260b6052efe
tteddy7 <tteddy7@me.com>
2019-02-13 21:11:02 -0600

    initial commit

---
+++ projects/HigherLower.py
@@ -0,0 +1,34 @@
+# Guessing game - try to guess the number
+
+# Import dependencies
+import random
+
+# Define function
+def higherLower():
+  # Let the user know the game is starting, give instructions
+  print('Guessing game!')
+  print('  A number between 1 and 100 will randomly be chosen.')
+  print('  Guess a number, and I will tell you if the real number is higher or lower.')
+
+  # Generate the number
+  numberToGuess = random.randint(1, 100)
+
+  # Set up a variable to see if they guessed correctly
+  guessCorrect = False
+
+  # Set up a variable to count how many guesses it took
+  guesses = 0
+
+  # Start the loop to let the user guess
+  while (guessCorrect == False):
+    guessedNumber = int(input('Guess a number: '))
+    guesses += 1
+    if (guessedNumber == numberToGuess):
+      print(f'Correct! You got the number in {guesses} guesses.')
+    elif (guessedNumber > numberToGuess):
+      print('Lower!')
+    elif (guessedNumber < numberToGuess):
+      print('Higher!')
+
+# Call the function
+higherLower()
