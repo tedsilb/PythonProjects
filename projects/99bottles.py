0d6b0359c856d9fd7ebc3d5fb16276bae88a35a9
tteddy7 <tteddy7@me.com>
2019-02-13 22:34:31 -0600

    add spacing

--- projects/99bottles.py
+++ projects/99bottles.py
@@ -11,6 +11,7 @@ def gen_lyrics():
     elif i == 1:
       print(f'{i} bottle of beer on the wall, {i} bottle of beer!')
       print('Take it down, pass it around, no more bottles of beer on the wall!')
+    
     else:
       print(f'{i} bottles of beer on the wall, {i} bottles of beer!')
       print(f'Take one down, pass it around, {i - 1} bottles of beer on the wall!')
