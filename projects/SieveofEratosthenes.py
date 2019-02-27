a6f6781ece65e8dd8d064752f3dd1e44c00d7314
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-02-27 12:03:26 -0600

    added sieve of eratosthenes

---
+++ projects/SieveofEratosthenes.py
@@ -0,0 +1,22 @@
+# This program will use the Sieve of Eratosthenes algorithm to find all prime numbers up to a given number.
+
+# Define function
+def sieveE(userNo):
+  numToGen = int(userNo)
+  numbersList = list(range(1, numToGen + 1))
+  for number in numbersList:
+    if number > 1:
+      numMult = number
+      for numMult in range(2, int(numToGen / number)):
+        try:
+          numbersList.remove(number * numMult)
+        except:
+          pass
+      numbersList
+  return numbersList
+
+# Gather number from user, run function
+print('This program will find all primes up to a given number.')
+enteredNo = input('Please enter a number: ')
+response = sieveE(enteredNo)
+print(response)
