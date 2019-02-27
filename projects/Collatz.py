f8f91e64d0f842ad4e38115c37bade76379f1039
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-02-27 12:05:23 -0600

    added step counter

--- projects/Collatz.py
+++ projects/Collatz.py
@@ -1,21 +1,22 @@
-'''
-  Collatz Conjecture
-  Start with a number n > 1.
-  Find the number of steps it takes to reach one using the following process:
-    If n is even, divide it by 2.
-    If n is odd, multiply it by 3 and add 1.
-'''
+# Collatz Conjecture
+# Start with a number n > 1.
+# Find the number of steps it takes to reach one using the following process:
+#   If n is even, divide it by 2.
+#   If n is odd, multiply it by 3 and add 1.
 
 # Define function
 def collatz(userNo):
   currentNo = int(userNo)
   print(currentNo)
+  steps = 0
   while currentNo != 1:
     if currentNo % 2 == 0:
       currentNo = int(currentNo / 2)
     else:
       currentNo = int(currentNo * 3) + 1
+    steps += 1
     print(currentNo)
+  print(f'Took {steps} steps.')
 
 # Call function
 print('This program will perform the Collatz conjecture on a given whole number.')
