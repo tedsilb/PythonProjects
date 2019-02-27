c55bc6f546b0476d06399321f8edaacec8af796f
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-02-27 12:41:46 -0600

    added bubble sort

---
+++ projects/BubbleSort.py
@@ -0,0 +1,43 @@
+# Super basic bubble sort algorithm
+
+# Define function to check to see if a list is sorted
+def listIsSorted(listToCheck):
+  for item in listToCheck:
+    itemIndex = listToCheck.index(item)
+    if itemIndex != len(listToCheck) - 1:
+      nextItemIndex = itemIndex + 1
+      nextItem = listToCheck[nextItemIndex]
+      if nextItem < item:
+        return False
+  return True
+
+# Define function to bubble sort
+def bubbleSort(userList):
+  listToSort = userList.split(',')
+  # First parse the numbers
+  for item in listToSort:
+    parseItem = float(item)
+    parseItemIndex = listToSort.index(item)
+    listToSort[parseItemIndex] = parseItem
+  # Then get down to business
+  while not listIsSorted(listToSort):
+    for item in listToSort:
+      itemIndex = listToSort.index(item)
+      # If not the last item in the list
+      if itemIndex != len(listToSort) - 1:
+        # Get the indexes
+        nextItemIndex = itemIndex + 1
+        nextItem = listToSort[nextItemIndex]
+        # If out of order,
+        if item > nextItem:
+          # Switch them
+          listToSort[itemIndex], listToSort[nextItemIndex] = listToSort[nextItemIndex], listToSort[itemIndex]
+      else:
+        pass
+  return listToSort
+
+# Get data from user, call function
+print('This program will bubble sort a given list of numbers.')
+inputList = input('Please enter some numbers, separated by commas, to be sorted: ')
+response = bubbleSort(inputList)
+print(response)
