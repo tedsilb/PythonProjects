992e0353bf8d27e6400ad6ac346354855f18c318
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-03-04 22:24:57 -0600

    changed to time execution and use dict for return value

--- projects/FibonacciCalc.py
+++ projects/FibonacciCalc.py
@@ -1,24 +1,36 @@
 # Generate the nth term in the Fibonacci sequence
 # By Ted Silbernagel
 
+# Import dependencies
+from time import time
+
 # Define function
 def fibonacciGen(nTerm):
+  # Get start time
+  startTime = time()
+
+  # Set up returnData object
+  returnData = {}
+
   # Check to see if they picked 1 or 2
   if nTerm <= 3:
     if nTerm == 1:
       fibNo = 0
-      # Return calculation for user
-      return f'The {nTerm}st number in the Fibonacci sequence is {fibNo}'
+      returnData['nTerm'] = nTerm
+      returnData['fibNo'] = fibNo
+      returnData['timeTaken'] = round(time() - startTime, 6)
 
     elif nTerm == 2:
       fibNo = 1
-      # Return calculation for user
-      return f'The {nTerm}nd number in the Fibonacci sequence is {fibNo}'
+      returnData['nTerm'] = nTerm
+      returnData['fibNo'] = fibNo
+      returnData['timeTaken'] = round(time() - startTime, 6)
 
     elif nTerm == 3:
       fibNo = 1
-      # Return calculation for user
-      return f'The {nTerm}rd number in the Fibonacci sequence is {fibNo}'
+      returnData['nTerm'] = nTerm
+      returnData['fibNo'] = fibNo
+      returnData['timeTaken'] = round(time() - startTime, 6)
 
   # Otherwise will need to generate
   else:
@@ -34,7 +46,11 @@ def fibonacciGen(nTerm):
       fibNo = prevNo1 + prevNo2
 
     # Return calculation for user
-    return f'The {nTerm}th number in the Fibonacci sequence is {fibNo}'
+    returnData['nTerm'] = nTerm
+    returnData['fibNo'] = fibNo
+    returnData['timeTaken'] = round(time() - startTime, 6)
+  
+  return returnData
 
 # Get n term from user
 print('This program will get you a specific number in the Fibonacci sequence.')
@@ -42,4 +58,12 @@ userNTerm = int(input('Please enter which number in the sequence you would like:
 
 # Call function, print response
 response = fibonacciGen(userNTerm)
-print(response)
+if response['fibNo'] == 1:
+  print(f'The {response["nTerm"]}st number in the Fibonacci sequence is {response["fibNo"]}')
+elif response['fibNo'] == 2:
+  print(f'The {response["nTerm"]}nd number in the Fibonacci sequence is {response["fibNo"]}')
+elif response['fibNo'] == 3:
+  print(f'The {response["nTerm"]}rd number in the Fibonacci sequence is {response["fibNo"]}')
+else:
+  print(f'The {response["nTerm"]}th number in the Fibonacci sequence is {response["fibNo"]}')
+print(f'Took {response["timeTaken"]}s.')
