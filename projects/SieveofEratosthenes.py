5d3b64cca9ffb2d716d29d15da4ea30a3d004f98
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-03-04 22:44:18 -0600

    added timer

--- projects/SieveofEratosthenes.py
+++ projects/SieveofEratosthenes.py
@@ -1,8 +1,12 @@
 # This program uses the Sieve of Eratosthenes algorithm to find all prime numbers up to a given number.
 # By Ted Silbernagel
 
+# Import dependencies
+from time import time
+
 # Define function
 def sieveE(userNo):
+  startTime = time()
   numToGen = int(userNo)
   numbersList = list(range(1, numToGen + 1))
   for number in numbersList:
@@ -14,10 +18,14 @@ def sieveE(userNo):
         except:
           pass
       numbersList
-  return numbersList
+  returnData = {}
+  returnData['numbersList'] = numbersList
+  returnData['timeTaken'] = round(time() - startTime, 6)
+  return returnData
 
 # Gather number from user, run function
 print('This program will find all primes up to a given number.')
 enteredNo = input('Please enter a number: ')
 response = sieveE(enteredNo)
-print(response)
+print(response['numbersList'])
+print(f'Took {response["timeTaken"]}s.')
