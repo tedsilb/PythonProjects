5a56528005ec9d9d54416195fbed8d83f7f1683e
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-05-03 21:29:08 -0500

    bugfix

--- projects/FizzBuzz.py
+++ projects/FizzBuzz.py
@@ -6,9 +6,9 @@ def fizzBuzz():
   for i in range(1, 101):
     if (i % 3) == 0 and (i % 5) == 0:
       print('FizzBuzz')
-    if (i % 3) == 0:
+    elif (i % 3) == 0:
       print('Fizz')
-    if (i % 5) == 0:
+    elif (i % 5) == 0:
       print('Buzz')
     else:
       print(i)
