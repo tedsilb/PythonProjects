0122dcee7e2eb898f16f2717d7ab55440489a350
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-06-09 21:30:37 -0500

    functionised, added ability to enter rolls/sides

--- projects/DiceRoller.py
+++ projects/DiceRoller.py
@@ -5,11 +5,29 @@
 import random
 
 # Set up dice-roll function
-def rollDice(n=1):  # default to 1 if they dont say how many times to run
-  print('Rolling dice ' + str(n) + ' times...')
+def rollDice(n, sides):  # default to 1 if they dont say how many times to run
+  # Parse user input
+  try:
+    n = int(n)
+  except ValueError:
+    n = 1
+  try:
+    sides = int(sides)
+  except:
+    sides = 6
+
+  # Roll the dice!
+  if n == 1:
+    print(f'Rolling a {str(sides)}-sided dice {str(n)} time...')
+  else:
+    print(f'Rolling a {str(sides)}-sided dice {str(n)} times...')
   for _ in range(0, n):
-    randomNo = random.randint(1, 6)
+    randomNo = random.randint(1, sides)
     print(randomNo)
 
-# Run the rollDice function three times.
-rollDice(3)
+# Gather data from user
+userSides = input('How many sides would you like the dice to have? (default: 6) ')
+userN = input('How many times would you like the dice to roll? (default: 1) ')
+
+# Run the rollDice function based on user input.
+rollDice(userN, userSides)
