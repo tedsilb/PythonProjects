55641bec8f92211db5d78426302f0a295ea1c575
tteddy7 <26172399+tedsilb@users.noreply.github.com>
2019-06-10 17:27:22 -0500

    reduce code

--- projects/CheckPalindrome.py
+++ projects/CheckPalindrome.py
@@ -1,18 +1,18 @@
 # A function to check if a word is a palindrome
 # By Ted Silbernagel
 
-def checkPalindrome(inputWord):
-  reversedWord = ''
-  for letter in reversed(inputWord):
-    reversedWord += letter
-  if inputWord == reversedWord:
-    print(f'Yes, {inputWord} is a palindrome.')
+def isPalindrome(inputWord):
+  if inputWord == inputWord[::-1]:
+    return True
   else:
-    print(f'No, {inputWord} is not a palindrome.')
+    return False
 
 # Ask user for a string to reverse
 print('This program will check to see if a given word is a palindrome.')
-stringToReverse = input('Please enter a word: ')
+stringToCheck = input('Please enter a word: ')
 
 # Call function, print results
-checkPalindrome(stringToReverse)
+if isPalindrome(stringToCheck):
+  print(f'Yes, {stringToCheck} is a palindrome.')
+else:
+  print(f'No, {stringToCheck} is not a palindrome.')
